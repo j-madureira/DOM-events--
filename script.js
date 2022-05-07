61fa7d820ca42e76d3f0a2a8878e8c424ec30d75
João Vitor Madureira <103207658+j-madureira@users.noreply.github.com>
2022-05-07 17:25:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+//Métodos card1
+function mudarCard1(){
+    var p1 = document.getElementById("p1");
+    p1.innerHTML =`<p>Lorem Ipsum is simply dummy text of the prin</p>`;
+}
+
+function voltarCard1(){
+    var p12 = document.getElementById("p1");
+    p12.innerHTML = "Veja sua mensagem!";
+}
+
+//Métodos card2
+function mudarCard2(){
+    var p2 = document.getElementById("p2");
+    p2.innerHTML = `<p>Lorem Ipsum is simply dummy text of the prin</p>`;
+}
+
+function voltarCard2(){
+    var p22 = document.getElementById("p2");
+    p22.innerHTML = "Veja sua mensagem!"
+}
+
+//Métodos card3
+function mudarCard3(){
+    var p3 = document.getElementById("p3");
+    p3.innerHTML = `<p>Lorem Ipsum is simply dummy text of the prin</p>`
+}
+
+function voltarCard3(){
+    var p33 = document.getElementById("p3");
+    p33.innerHTML = "Veja sua mensagem!"
+}
